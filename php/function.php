e587cb2d308d028250f530c6573b1672063b847f
MaxMax89 <chip_pro89@mail.ru>
2022-09-03 20:37:28 +0300

    Добавил примеры работы с функциями

--- php/function.php
+++ php/function.php
@@ -66,6 +66,125 @@ function one_function($num) {
    }
   echo "сегодня".  .weekday(3);  // сегодня среда
 
+function debug($data) {
+  echo '<pre>' . print_r($data, 1) . '</pre>';
+}
+
+ function get_digits($num){
+  return str_split($num, 1);
+ }
+
+function arrey_sum($arr){
+  $sum = 0;
+  foreach ($arr as  $value) {
+    $sum  += $value;
+  }
+}
+
+function in_range($num){
+  $sum = arrey_sum(get_digits($num));
+  return $sum >= 1 and $sum <= 9;
+}
+
+
+$array = [15,16,22,824,436,];
+  $result[] = 0;
+  foreach ($array as  $value) {
+    $sum += array_sum(get_digits($value));
+  }
+echo $sum;
+
+
+////////////////////////////////                     Функция inArray
+//Задача: реализуйте функцию inArray, которая будет проверять, есть ли в массиве элемент с таким значением или нет. Первым параметром функция должна принимать значение, а вторым - массив. Если такой элемент есть - функция должна вернуть true, а если нет - false.
+
+
+
+
+function inArray($num, $array){
+  foreach ($array as $value) {
+    if ($num == $value) {
+      return true;     
+    } 
+      return false;
+    
+  }
+}
+
+$arrayNum = [15,22,458,333,21,98];
+  if  inArray(333,$arrayNum) === true {
+    echo "Yes";
+
+  } else{
+    echo "No";
+  }
+    
+         //Задача: реализуйте функцию isSimple, которая параметром будет принимать число и проверять, простое оно или нет. Простое число - это число, которое не делится ни на одно другое число (кроме как на 1 и на само себя - на это делятся все числа). Если число простое - функция должна вернуть true, в противном случае false.
+
+  function isSimple($num){
+    for ($i=2; $i < $num ; $i++) { 
+       if ($num % $i == 0){
+        return false;
+       }
+        return true;
+    } 
+
+  }
+echo isSimple(13); // выведет true
+
+
+           //Задача: реализуйте функцию getDivisors, которая параметром будет принимать число и возвращать массив его делителей, то есть чисел, на которое делится наше число. К примеру, если мы передадим число 24 - мы должны получить массив [1, 2, 3, 4, 6, 12, 24].
+
+function getDivisort($num){
+$result = [];  
+for ($i=1; $i <= $num; i++) { 
+    if ($num % $i == 0){
+      $result[] = $num;
+    }
+  }
+   return $result;
+}
+
+echo get_digits(24);
+                                      //Static переменные
+
+  //Создадим простую функцию:
+  function func()
+  {
+    /*
+      Укажем PHP, что мы хотим, чтобы $a была статической переменной:
+    */
+    static $a;
+
+    $a++;
+    echo $a;
+  }
+
+  func(); //выведется 1
+  func(); //выведется 2!
+  func(); //выведется 3
+
+
+
+
+
+
+                                             //Рекурсия
+                //В программировании есть такое понятие, как рекурсия - это когда функция вызывает сама себя
+
+
+$arr = [1, 2, 3, 4, 5];
+
+  last($arr);
+
+  function last($arr)
+  {
+    echo array_pop($arr).'<br>'; //выводим последний элемент массива
+    
+    if(!empty($arr)) {
+      last($arr); //это рекурсия
+    }
+  }
 
 
 
